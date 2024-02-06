d0107fa3ba67db666d8277487c70fb5f611fa150
Tiago Santini <tiagosantiniathayde@gmail.com>
2024-02-06 14:58:17 -0300

    add: initial layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarkdownComponent, MarkdownModule } from 'ngx-markdown';
+import { MatCardModule } from '@angular/material/card';
+
+import { ShellComponent } from './core/shell/shell.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarkdownComponent, MatCardModule, ShellComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'markdown-blog-app';
